6b3d23c30b07c3a733415e91684ac915a0239fcf
John Dimeo <dimeo@elderresearch.com>
2019-03-12 07:36:28 -0400

    Add a utility to make unit file capitalization consistent with a Caps-Hyphen naming

---
+++ src/main/java/dimeo/triplea/CorrectFileCapitalization.java
@@ -0,0 +1,37 @@
+package dimeo.triplea;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.text.WordUtils;
+
+import lombok.val;
+
+public class CorrectFileCapitalization {
+
+	public static void main(String[] args) throws IOException {
+		val root = Paths.get(args[0]);
+		Files.walk(root).forEach(f -> {
+			if (Files.isDirectory(f)) { return; }
+			
+			String orig = f.toString();
+			int i = orig.lastIndexOf(File.separatorChar);
+			String fixed = orig.substring(0, i + 1) + WordUtils.capitalizeFully(orig.substring(i + 1), '-', ' ').replace("Aa", "AA");
+			if (!StringUtils.equals(orig, fixed)) {
+				System.out.println(orig + " -> " + fixed);
+				try {
+					Path tmp = Paths.get(orig.substring(0, i + 1) + "windows-is-dumb-sometimes.png");
+					Files.move(f, tmp);
+					Files.move(tmp, Paths.get(fixed));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}	
+			}
+			
+		});
+	}
+}
