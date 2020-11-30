5d7e7506bffa54d677a9262c499af9b874f00a8b
John Dimeo <dimeo@elderresearch.com>
2020-11-29 22:47:28 -0500

    Add CSS inlining to note copying

--- src/main/java/games/strategy/util/CopyNotes.java
+++ src/main/java/games/strategy/util/CopyNotes.java
@@ -1,9 +1,21 @@
 package games.strategy.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
+
+import org.fit.cssbox.css.CSSNorm;
+import org.fit.cssbox.css.DOMAnalyzer;
+import org.fit.cssbox.css.NormalOutput;
+import org.fit.cssbox.io.DefaultDOMSource;
+import org.fit.cssbox.io.StreamDocumentSource;
+import org.xml.sax.SAXException;
+
+import com.google.common.net.MediaType;
 
 import lombok.val;
 import picocli.CommandLine.Command;
@@ -23,7 +35,7 @@ public class CopyNotes implements Callable<Void> {
 	private Path notesFile;
 	
 	@Override
-	public Void call() throws IOException {
+	public Void call() throws IOException, SAXException {
 		if (notesFile == null) {
 			notesFile = gameFile.getParent().getParent().resolve("doc").resolve("notes.html");
 		}
@@ -33,8 +45,24 @@ public class CopyNotes implements Callable<Void> {
 		int j = s.indexOf("]]>", i);
 
 		if (importNotes) {
-			val html = Files.readString(notesFile);
-			Files.writeString(gameFile, s.substring(0, i) + html + s.substring(j));
+			// Strip out assumed image path since TripleA assumes (/doc/images)
+			val html = Files.readString(notesFile).replace("src=\"images/", "src=\"");
+			
+	        // Inline CSS styles 
+			val bais = new ByteArrayInputStream(html.getBytes(StandardCharsets.UTF_8));
+			val src = new StreamDocumentSource(bais, null, MediaType.HTML_UTF_8.toString());
+	        val doc = new DefaultDOMSource(src).parse();
+	        val da = new DOMAnalyzer(doc);
+	        da.attributesToStyles();
+	        da.addStyleSheet(null, CSSNorm.userStyleSheet(), DOMAnalyzer.Origin.AGENT);
+	        da.getStyleSheets();
+	        da.stylesToDomInherited();
+	        
+	        val baos = new ByteArrayOutputStream();
+	        new NormalOutput(doc).dumpTo(baos);
+	        val inlinedHtml = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+			
+			Files.writeString(gameFile, s.substring(0, i) + inlinedHtml + s.substring(j));
 		} else {
 			Files.writeString(notesFile, s.substring(i, j));
 		}
