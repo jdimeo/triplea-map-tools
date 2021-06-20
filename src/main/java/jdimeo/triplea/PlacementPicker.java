2dfad85c16757cd908462e024a41fbe12b0bbf50
John Dimeo <dimeo@elderresearch.com>
2021-06-19 23:58:37 -0400

    Do the placement search at different starting offsets and use the one that yields the most placements

--- src/main/java/jdimeo/triplea/PlacementPicker.java
+++ src/main/java/jdimeo/triplea/PlacementPicker.java
@@ -3,13 +3,15 @@ package jdimeo.triplea;
 import java.awt.Point;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import org.jooq.lambda.Seq;
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Envelope;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.util.GeometricShapeFactory;
 
@@ -27,7 +29,7 @@ import picocli.CommandLine.Parameters;
 	"This differs from existing tools because it packs circles, not squares, since often the corners of unit images can overlap without causing visual problems."
 })
 public class PlacementPicker implements Callable<Void> {
-	private static final double CIRCLE_PACK_DENSITY_HEX = 0.908699;
+	private static final double CIRCLE_PACK_DENSITY_HEX = 0.91;
 	
 	@Parameters(description = "The folder containing centers.txt, polygons.txt and the place to write place.txt")
 	private Path mapFolder;
@@ -35,10 +37,16 @@ public class PlacementPicker implements Callable<Void> {
 	@Option(names = {"-d", "--diameter"}, description = "The diameter of the unit placement circles")
 	private int diameter = 60;
 	
+	@Option(names = {"-s", "--step-size"}, description = "The step size of the offset from (0, 0) to try to maximize placements per territory (up to the radius)")
+	private int step = 3;
+	
+	private List<TerritoryGeo> territories;
+	private GeometricShapeFactory factory;
+	
 	@Override
 	public Void call() throws Exception {
 		log.info("Loading polygons...");
-		val territories = TerritoryGeo.fromPolysFile(mapFolder.resolve("polygons.txt"));
+		territories = TerritoryGeo.fromPolysFile(mapFolder.resolve("polygons.txt"));
 
 		log.info("Loading centers...");
 		try (val is = Files.newInputStream(mapFolder.resolve("centers.txt"))) {
@@ -46,36 +54,30 @@ public class PlacementPicker implements Callable<Void> {
 			territories.forEach($ -> $.setCenter(centers.get($.getName())));
 		}
 
-		val factory = new GeometricShapeFactory(TerritoryGeo.GEO_FACTORY);
+		factory = new GeometricShapeFactory(TerritoryGeo.GEO_FACTORY);
 		factory.setSize(diameter);
 		
-		val ystep  = diameter * CIRCLE_PACK_DENSITY_HEX;
 		val radius = diameter * 0.5;
 		
 		log.info("Computing placements...");
-		territories.forEach(t -> {
-			val geo        = t.getGeo();
-			val env        = geo.getEnvelopeInternal();
-			val center     = geo.getCentroid();
-			val placements = t.getPlacements();
+		territories.parallelStream().forEach(t -> {
+			val geo    = t.getGeo();
+			val env    = geo.getEnvelopeInternal();
+			val center = geo.getCentroid();
 			
-			for (double x = env.getMinX() + (center.getX() % diameter); x <= env.getMaxX(); x += diameter) {
-				int row = 0;
-				for (double y = env.getMinY() + (center.getY() % diameter); y <= env.getMaxY(); y += ystep) {
-					double xoffset = x + (row % 2) * 0.5 * diameter;
-					factory.setBase(new Coordinate(xoffset, y));
-					val placement = factory.createCircle();
-					
-					// Make sure the placement isn't contained by any *other* territory, which
-					// can happen if one territory is fully contained within another
-					if (contains(geo, placement) && Seq.seq(territories).noneMatch($ -> overlaps($.getGeo(), placement))) {
-						t.getPlacements().add(round(xoffset, y));
+			// Try a some starting offsets to see which yields the most placements
+			int max = 0;
+			for (double x = 0; x <= radius; x += step) {
+				for (double y = 0; y <= radius; y += step) {
+					val placements = findPlacements(t, env, x, y);
+					if (placements.size() > max) {
+						max = placements.size();
+						t.setPlacements(placements);
 					}
-					row++;
 				}
 			}
-			if (placements.isEmpty()) {
-				placements.add(round(center.getX() - radius, center.getY() - radius));
+			if (max == 0) {
+				t.getPlacements().add(round(center.getX() - radius, center.getY() - radius));
 			}
 			
 			// Put placement closest to center first
@@ -88,6 +90,28 @@ public class PlacementPicker implements Callable<Void> {
 		
 		log.info("Done.");
 		return null;
+	}
+	
+	private List<Point> findPlacements(TerritoryGeo t, Envelope env, double startX, double startY) {
+		val ystep  = diameter * CIRCLE_PACK_DENSITY_HEX;
+		
+		val ret = new ArrayList<Point>();
+		for (double x = env.getMinX() + startX; x <= env.getMaxX(); x += diameter) {
+			int row = 0;
+			for (double y = env.getMinY() + startY; y <= env.getMaxY(); y += ystep) {
+				double xoffset = x + (row % 2) * 0.5 * diameter;
+				factory.setBase(new Coordinate(xoffset, y));
+				val placement = factory.createCircle();
+				
+				// Make sure the placement isn't non-trivially overlapping *another* territory,
+				// which can happen if one territory is fully contained within another
+				if (contains(t.getGeo(), placement) && Seq.seq(territories).filter($ -> $ != t).noneMatch($ -> overlaps($.getGeo(), placement))) {
+					ret.add(round(xoffset, y));
+				}
+				row++;
+			}
+		}
+		return ret;
 	}
 	
 	private static Point round(double x, double y) {
